7a33ccb9509916b17556ee38e77f63e4d52d94e0
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-14 09:03:33 -0700

    Updated 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point to the program.
+ * Return - 0 for success
+ */
+
 int main()
 {
 	puts("programming is like building a multilingual puzzle");
-	return(0)
+	return (0);
 }
