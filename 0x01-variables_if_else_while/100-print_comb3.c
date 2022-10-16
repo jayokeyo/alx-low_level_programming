ebefcc581d33fa37f2ac74b26b7d74d2bec65e50
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-16 14:34:57 -0700

    Added 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,7 +13,7 @@ int main(void)
 
 	for (num1 = 0; num1 < 9; num1++)
 	{
-		for (num2 = num1 + 1; num <= 9; num++)
+		for (num2 = num1 + 1; num2 <= 9; num2++)
 		{
 			if (num1 != 8 && num2 != 9)
 			{
