b5fd3db705a42b2991c97b2d5f9d6a2e75fee95a
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-16 15:46:16 -0700

    Added 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,36 +10,24 @@ int main(void)
 {
 	int num1;
 	int num2;
-	/* int num3;
-	int num4;
-	*/
 	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (num2 = 0; num2 <= 99; num2++)
+		for (num2 = 1; num2 <= 99; num2++)
 		{
-			putchar(num1 + '0');
-			putchar(num2 + '0');
-			/*for (num3 = 0; num3 <= 9; num3++)
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num1 == 99 && num2 == 99)
 			{
-				for (num4 = 1; num4 <= 9; num4++)
-				{
-					putchar(num1 + '0');
-					putchar(num2 + '0');
-					putchar(' ');
-					putchar(num3 + '0');
-					putchar(num4 + '0');
-					if (num1 == 9 && num2 == 9 && num3 == 9 && num4 == 9)
-					{
-						putchar('\n');
-					}
-					else
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+				putchar('\n');
 			}
-			*/
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	return (0);
