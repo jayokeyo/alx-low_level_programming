1c929fb954f7150f1a18875ca8f2a03d74451bee
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-16 14:48:14 -0700

    Added 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,14 +17,14 @@ int main(void)
 		{
 			putchar(num1 + '0');
 			putchar(num2 + '0');
-			if (num1 != 8 && num2 != 9)
+			if (num1 == 8 && num2 == 9)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar('\n');
 			}
 			else
 			{
-				putchar('\n');
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
