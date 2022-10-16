457cf8e1c899a59fe76cf73ccf8c6767beafb6c9
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-16 12:39:31 -0700

    Added 2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,15 +2,17 @@
 
 /**
  * main - Entry point.
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success).
  */
 
 int main(void)
 {
 	char alpha;
 	 
-	for (alpha = 'a';alpha <= 'z';alpha++)
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
 		putchar(alpha);
+	}
 	putchar('\n');
 
 	return (0);
