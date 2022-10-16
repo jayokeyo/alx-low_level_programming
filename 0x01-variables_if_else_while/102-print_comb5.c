a00463cff2ef3d9ca42d12132cf836e39e11ef3c
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-16 15:57:31 -0700

    Added 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,6 +10,7 @@ int main(void)
 {
 	int num1;
 	int num2;
+
 	for (num1 = 0; num1 <= 98; num1++)
 	{
 		for (num2 = num1 + 1; num2 <= 99; num2++)
@@ -19,7 +20,7 @@ int main(void)
 			putchar(' ');
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
-			if (num1 == 99 && num2 == 99)
+			if (num1 == 98 && num2 == 99)
 			{
 				putchar('\n');
 			}
