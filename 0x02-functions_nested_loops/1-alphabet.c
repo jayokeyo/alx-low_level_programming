f17629e5295c54834aab9157c4e8486c388c4d63
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-18 12:49:00 -0700

    Added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints an array of characters
+ * (letters of the alphabet)
+ * 
+ * Return: Nothing (Success)
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+	return
+}
