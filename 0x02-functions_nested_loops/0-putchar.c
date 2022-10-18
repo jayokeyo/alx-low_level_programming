685309d5388007893085f3ca70eeeaabc8b15dcc
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-18 11:47:40 -0700

    Added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: Returns nothing
+ */
+void main(void)
+{
+	printf("_putchar\n");
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Basic programs on C functions and nested loops
