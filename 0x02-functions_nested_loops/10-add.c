90703aa479564918961f38f3ed0adec4506f3b8e
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-19 05:00:37 -0700

    Added 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Evaluates the sum of two integers
+ * num1: First integer
+ * num2: Second integer
+ * Return: Sum of num1 and num2
+ */
+int sum(int num1, int num2)
+{
+	return (num1 + num2);
+}
