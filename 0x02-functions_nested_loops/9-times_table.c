e87de9ee4ffb2e46dc6409df839baf0c48fe9b5f
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-19 04:55:21 -0700

    Updated 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,10 @@ void times_table(void)
 
 	for (num1 = 0; num1 <= 9; num1++)
 	{
-		for (num2 = 0; num2 < 9; num2++)
+		_putchar(0 + '0');
+		_putchar(',');
+		_putchar(' ');
+		for (num2 = 1; num2 < 9; num2++)
 		{
 			if ((num1 * num2) <= 9)
 			{
