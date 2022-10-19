0b835cc9b76ecc6728e132e847c15b99536f40d0
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-19 15:22:35 -0700

    Added 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+	int sum = 1;
+	int b = 0;
+	int a = 0;
+
+	printf("%d, ", sum);
+	for (c = 1; c < 50; c++)
+	{
+		a = b;
+		b = sum;
+		sum = a + b;
+		printf("%d, ", sum);
+	}
+}
+}
