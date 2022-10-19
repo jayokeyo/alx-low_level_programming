54001ecb89143407cf78c67015e8259723253877
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-19 05:06:17 -0700

    Described parameters

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add - Evaluates the sum of two integers
- * num1: First integer
- * num2: Second integer
+ * @num1: First integer
+ * @num2: Second integer
  * Return: Sum of num1 and num2
  */
 int add(int num1, int num2)
