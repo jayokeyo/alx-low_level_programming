a764bafc14d31e76b7911ebbf3a63ed28693fd5c
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-19 16:13:58 -0700

    Added 103-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,12 +5,12 @@
  */
 int main(void)
 {
-	long int fib = 1;
-	long int b = 0;
-	long int a = 0;
+	unsigned long int fib = 1;
+	unsigned long int b = 0;
+	unsigned long int a = 0;
 	int c;
 
-	for (c = 1; c <98; c++)
+	for (c = 1; c < 98; c++)
 	{
 		a = b;
 		b = fib;
