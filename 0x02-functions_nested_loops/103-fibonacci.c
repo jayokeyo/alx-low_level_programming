4327e039cda635392148facf434dc4b41944da20
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-19 15:57:19 -0700

    Added 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int fib = 1;
+	long int b = 1;
+	long int a = 0;
+	long int sum =  0;
+
+	while (sum <= 4000000)
+	{
+		a = b;
+		b = fib;
+		fib = a + b;
+		if (fib % 2 == 0)
+		{
+			sum += fib;
+			printf("%ld\n", sum);
+		}
+	}
+	return (0);
+}
