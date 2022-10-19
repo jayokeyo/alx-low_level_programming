b9a5f066819d55c93a66aadb97b79fa8a40ac599
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-19 14:23:30 -0700

    Added 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 {
 	int num1, num2;
 
-	if (n >= 0 && n <= 15)
+	if (n > 0 && n <= 15)
 	{
 		for (num1 = 0; num1 <= n; num1++)
 		{
@@ -17,37 +17,21 @@ void print_times_table(int n)
 			for (num2 = 1; num2 < n; num2++)
 			{
 				if ((num1 * num2) <= 9)
-				{
 					printf("  %d, ", (num1 * num2));
-				}
-				else
-				{
-					if ((num1 * num2) <= 99)
-					{
-						printf(" %d, ", (num1 * num2));
-					}
-					else
-					{
-						printf("%d, ", (num1 * num2));
-					}
-				}
+				if ((num1 * num2) <= 99)
+					printf(" %d, ", (num1 * num2));
+				if ((num1 * num2) > 99)
+					printf("%d, ", (num1 * num2));
 			}
 			if ((num1 * n) <= 9)
-			{
 				printf("  %d", (num1 * n));
-			}
-			else
-			{
-				if ((num1 * n) <= 99)
-				{
-					printf(" %d", (num1 * n));
-				}
-				else
-				{
-					printf("%d", (num1 * n));
-				}
-			}
+			if ((num1 * n) <= 99)
+				printf(" %d", (num1 * n));
+			if ((num1 * n) > 99)
+				printf("%d", (num1 * n));
 			printf("\n");
 		}
 	}
+	if (n == 0)
+		printf("%d", 0);
 }
