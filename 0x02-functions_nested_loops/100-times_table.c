ceb304734f70a3317d7671cb54026625d0bec4e5
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-19 12:57:10 -0700

    Updated main.h

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 {
 	int num1, num2;
 
-	if (n > 0 && n < 15)
+	if (n > 0 && n <= 15)
 	{
 		for (num1 = 0; num1 <= n; num1++)
 		{
@@ -18,7 +18,7 @@ void print_times_table(int n)
 			_putchar(' ');
 			for (num2 = 1; num2 < n; num2++)
 			{
-				if ((num1 * num2) <= 9)
+				if ((num1 * num2) <= n)
 				{
 					_putchar(' ');
 					_putchar((num1 * num2) + '0');
