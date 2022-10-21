8dbb370a59416d1081d4e39ac5cb2ed99cd80d3f
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-21 15:26:12 -0700

    Added 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_square - draws a square of side size using #
+ * @size: Size of the square drawn.
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (c = 0; c < size; c++)
+		{
+			for (c = 0; c < size; c++)
+				_putchar("#");
+			_putchar('\n');
+		}
+	}
+}
