44981feea7f1cbf54dd9565dcac60f4b373e0130
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-21 13:44:34 -0700

    Updated 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - Uses underscores (_) to print line
+ * @n: Integer input to print_line
+ */
+void print_line(int n)
+{
+	int c;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (c = 1; c <= n; c++)
+			_putchar('_');
+		_putchar('\n');
+	}
