b2e74d102981b4bb7a35786962e983bbf2704b56
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-21 17:29:57 -0700

    Added 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,17 @@ int main(void)
 	for (c = 1; c <= 100; c++)
 	{
 		if ((c % 3 == 0) && (c % 5 == 0))
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else
 		{
 			if (c % 3 == 0)
-				printf("Fizz");
+				printf("Fizz ");
 			else
 			{
 				if (c % 5 == 0)
-					printf("Buzz");
+					printf("Buzz ");
 				else
-					printf("%d", c);
+					printf("%d ", c);
 			}
 		}
 	}
