16f16a23fbefb5c88b91d4850df50ebbc3c12454
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-21 17:17:40 -0700

    Added 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints integers between 1 and 100
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+	 for (c = 1, c <= 100; c++)
+	 {
+		 if ((c % 3 == 0) && (c % 5 == 0))
+			 printf("FizzBuzz");
+		 else
+		 {
+			 if (c % 3 == 0)
+			 	printf("Fizz");
+			 else
+			 {
+				 if (c % 5 == 0)
+				 	printf("Buzz");
+				 else
+					 printf("%d", c);
+			 }
+		 }
+	 }
+}
