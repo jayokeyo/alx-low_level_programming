8c7d8fe35e8cbc56e750a3e8d874d02db4a31e61
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-25 15:05:12 -0700

    Added 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,44 @@
+@include "main.h"
+/**
+ * _atoi - scans through a string and converts it to string
+ * @s: string input
+ * Return: Converted integer if string has one otherwise 0
+ */
+int _atoi(char *s)
+{
+	int a, j = 0;
+	char *ns1, *ns2;
+
+	ns1[0] = "+";
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] == "+" || s[a] == "-")
+		{
+			if ((ns1[0] == "+" && s[a] == "-") || (ns1[0] == "-" && s[a] == "+"))
+				ns1[0] = "-";
+			else
+				ns1[0] = "+";
+		}
+		if (s[a] >= 0 && s[a] <= 9)
+			break;
+	}
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] >= 0 && s[a] <= 9)
+		{
+			j++;
+			ns1[j] = s[a];
+		}
+	}
+	if (ns1[1] != '\0' && ns1[0] == "-")
+		return (ns1);
+	if (ns1[1] != '\0' && ns1[0] == "+")
+	{
+		for (a = 1; ns1[a] != '\0'; a++)
+			ns2[a - 1] = ns1[a];
+		return (ns2);
+	}
+	if (ns1[1] == '\0')
+		return (0);
+}
