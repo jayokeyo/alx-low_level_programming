8be92abc9c30e602d42e7758d0205a6cb4142a6b
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-25 10:13:54 -0700

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,11 +5,12 @@
  */
 void print_rev(char *s)
 {
-	int i, len = 0;
+	int i, len;
+	len = 0;
 
 	for (i = 0, s[i] != '\0', i++)
 		len++;
-	for (i = (len -1); i >=0; i++)
+	for (i = (len - 1); i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
