9ca8618a68fc5bca371bc97077f640ba35ed775c
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-25 12:56:49 -0700

    Added 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *_strcpy - copies the content of one memory location to a buffer
+ * @dest: pointer to destination
+ * @src: pointer to source
+ * Return: Address to destination.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] == '\0'; i++)
+		dest[i] = src[i];
+	return (dest)
+}
