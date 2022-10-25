652613e951a016eb93ba682f0529e251318deb77
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-25 13:30:25 -0700

    Updated 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,10 +7,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i, len = 0;
 
 	for (i = 0; src[i] != '\0'; i++)
-		dest[i] = src[i];
-	dest[i + 1] = '\0';
+		len++;
+	if (len > 0)
+	{
+		for (i = 0; src[i] != '\0'; i++)
+			dest[i] = src[i];
+		dest[i + 1] = '\0';
+	}
 	return (dest);
 }
