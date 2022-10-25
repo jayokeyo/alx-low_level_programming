ba97746fbf677c726debf2aa10d30054c9dbf09d
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-25 11:46:55 -0700

    Added 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - prints characters at even positions
+ * @str: string input to the function
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(i);
+		i++;
+	}
+}
