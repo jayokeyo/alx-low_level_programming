bdf82b89481bcf11a3ec536e94d834101e12555d
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-25 10:29:24 -0700

    Added 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - reverses an input string
+ * @s: input string
+ */
+void rev_string(char *s)
+{
+	int i, j = 0, len = 0;
+	char *rev;
+
+	for (i = 0; sum[i]; i++)
+		len++;
+	for (i = (len -1); i >= 0; i--)
+	{
+		rev[j] = s[i];
+		j++;
+	}
+	return (*rev);
+}
