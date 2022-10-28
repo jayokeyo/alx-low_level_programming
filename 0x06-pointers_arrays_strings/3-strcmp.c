2435153206000de6ee149969c54f2f5451c30b86
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-28 12:03:52 -0700

    Updated strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,9 +13,7 @@ int _strcmp(char *s1, char *s2)
 		len1++;
 	for (a = 0; s2[a] != '\0'; a++)
 		len2++;
-	/*if (len1 == 0 || len2 == 0)
-		r = 0;*/
-	if (len1 >= len2 && len2 != 0)
+	if (len1 >= len2)
 	{
 		for (a = 0; s1[a] != '\0'; a++)
 		{
@@ -31,7 +29,7 @@ int _strcmp(char *s1, char *s2)
 			}
 		}
 	}
-	if (len2 > len1 && len1 != 0)
+	if (len2 > len1)
 	{
 		for (a = 0; s2[a] != '\0'; a++)
 		{
