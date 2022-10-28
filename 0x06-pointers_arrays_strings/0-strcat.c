3cf6103858fdf97a3c209312e43b2a4b50055361
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-28 00:30:08 -0700

    Added 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcat - contactenate a source string to a destination string
+ * @src: source string
+ * @dest: destination string
+ * Return: Pointer to the destination string
+ */
+char _strcat(char *dest, char *src)
+{
+	int a;
+	int len = 0;
+
+	for (a = 0; dest[a] != '\0'; a++)
+		len++;
+	for (a = (len + 1); src[a - len] != '\0'; a++)
+		dest[a] = src[a - len];
+	return (dest);
+}
