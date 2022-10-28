f1fd7232d6316c02760db2d98c4545a5e65901ff
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-28 12:15:22 -0700

    Added 4-rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reverse_array - reverses an input array
+ * @a: input array
+ * @n: number of elements in a
+ */
+void reverse_array(int *a, int n)
+{
+	for (int b = 0; b < n; b++)
+		c[b] = a[n - b - 1];
+	for (int d = 0; d < n; d++)
+		a[d] = c[d];
+}
