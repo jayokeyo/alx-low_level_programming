77e50fe0d42680883a3b0ccfbd55c94cb5a70151
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-01 05:13:25 -0700

    Added 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - checks a string for accepted characters
+ * @s: pointer to input string
+ * @accept: accepted characters
+ * Return: Number of bytes before an unaccepted char is met
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int a, b, counter = 0, flag = 1;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; accept[b] != '\0'; b++)
+		{
+			if (s[a] == accept[b])
+			{
+				counter++;
+				break;
+			}
+			else
+				flag = 0;
+		}
+		if (flag == 0)
+			break;
+	}
+	return (counter);
+}
