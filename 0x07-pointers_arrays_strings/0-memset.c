53cff929a07556aeb5d2b1100cb872f3b2c91e68
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-01 03:41:57 -0700

    Updated 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,9 +3,10 @@
  * _memset - sets the value of a memory location to b
  * @s: pointer to memory location
  * @b: byte to be stored onto the memory location
- * @n: numner of bytes of b to be stored
+ * @n: number of bytes of b to be stored
+ * Return: Always the address of the string array
  */
-char * _memset(char * s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int a;
 
