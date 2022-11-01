83b3fd70e520f4faebf7b27eafe074249b83effe
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-01 05:34:56 -0700

    Added 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,9 +13,10 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (b = 0; accept[b] != '\0'; b++)
 		{
-			if (s[a] == accept[b] || s[a] == ' ' || s[a] == ',')
+			if (s[a] == accept[b])
 			{
 				counter++;
+				flag = 1;
 				break;
 			}
 			else
