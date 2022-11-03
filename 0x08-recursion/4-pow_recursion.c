7bf93c343a769547bbdeabbe3355fe1245dcc490
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-03 04:37:08 -0700

    Added 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _pow_recursion - evaluates pow(x, y)
+ * @x: base integer
+ * @y: power index
+ * Return: x raised to y
+ */
+int _pow_recursion(int x, int y)
+	return (x ** y);
