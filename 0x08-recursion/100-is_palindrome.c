aaf7331942fff5d1c8107389346ba42338d82cb0
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-03 08:23:41 -0700

    Added 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,28 +4,44 @@
  * @s: input string
  * Return: 1 if s is a palindrome otherwise 0;
  */
+int find_len(char *s);
+int comp_char(char *s);
+int is_palindrome(char *s);
+
 int i = 0;
 
+/**
+ * find_len - evaluates the length of string s
+ * @s: input string
+ * Return: length of s
+ */
 int find_len(char *s)
 {
 	int len = 0;
 
-	if (s*)
+	if (*s)
 	{
 		len++;
 		s++;
 	}
 	return (len);
 }
+
+/**
+ * comp_char - compares corresponding in s
+ * @s: input string
+ * Return: 1 if s is a palindrome otherwise 0
+ */
 int comp_char(char *s)
 {
 	int len = find_len(s);
+	int ret;
 
 	if (s[i] == s[len - i])
 	{
 		comp_char(s);
 		i++;
-		ret = 1
+		ret = 1;
 	}
 	else
 		ret = 0;
@@ -33,12 +49,12 @@ int comp_char(char *s)
 }
 int is_palindrome(char *s)
 {
-	len = find_len(*s);
+	len = find_len(s);
 	if (len == 0)
 		pal = 1;
 	if (len == 1)
 		pal = 0;
 	else
-		pal = comp_char(*s);
+		pal = comp_char(s);
 	return (pal);
 }
