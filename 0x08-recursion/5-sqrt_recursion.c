f9e94d84ab23e37d169fa32b3183bf14f56b88ac
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-03 05:18:43 -0700

    Added 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,22 +4,22 @@
  * @n: number whose square root is evaluated
  * Return: Natural square root otherwise -1
  */
-int _sqrt(int n, int i = 2);
+int _sqrt(int n, int i);
 
 int _sqrt_recursion(int n)
 {
+	int i = 2;
+
 	if (n <= 0)
 		return (-1);
 	if (n == 1)
 		return (1);
 	if (n > 1)
-		return (_sqrt(n));
+		return (_sqrt(n, i));
 }
-int _sqrt(int n, int i = 2)
+int _sqrt(int n, int i)
 {
-	int i = 2;
-
-	if (i * i = n)
+	if (i * i == n)
 		sqrt = i;
 	else
 	{
