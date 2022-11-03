92d324b705d7bb52ef1602553915ad8200f520af
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-03 00:35:11 -0700

    Added 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: input string
+ */
+void _puts_recursion(char *s)
+{
+	-Putchar(s[0]);
+	s++;
+	_puts_recursion(char *s);
+}
