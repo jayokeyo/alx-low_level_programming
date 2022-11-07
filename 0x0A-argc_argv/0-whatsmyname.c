c49eb0bed74d05322904324ef04d25bd32b2671b
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-07 04:31:49 -0800

    Added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints name of the program
+ * @argc: number of arguments
+ * @argv: pointer to array of arguments
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
