0d6e7f538a33bb983545462bfedbc6ae4b3437fb
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-07 07:18:10 -0800

    Added 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - evaluates the number of cents coins
+ * @argc; number of arguments
+ * @argv: pointer array to arguments
+ * Return: 0 success otherwise 1
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	int ret, rem, sum = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		ret = 1;
+	}
+	else
+	{
+		sum += atoi(argv[1]) / 25;
+		rem = atoi(argv[1]) % 25;
+		sum += rem / 10;
+		rem = rem % 10;
+		sum += rem / 5;
+		rem = rem % 5;
+		sum += rem / 2;
+		rem = rem / 2;
+		sum += rem;
+		printf("%d\n", sum);
+		ret = 0;
+	}
+	return (ret);
+}
