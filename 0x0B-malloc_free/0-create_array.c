7d942011f5a97f49704599e26903e42bb8275b63
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-09 07:31:19 -0800

    Updated 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 		pty = (char*)malloc(size);
 		for (i = 0; i < size; i++)
 			*(pty + size) = c;
-		ret = pty;
+		*ret = pty;
 	}
 	else
 		ret = NULL;
