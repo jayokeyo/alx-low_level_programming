a07f8381041ab5256f5efae3096770ddfb123741
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-10 12:28:43 -0800

    Updated 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@ int **alloc_grid(int width, int height)
 {
 	int i, j;
 	int **ret;
-	int **ptr = (int **)malloc((sizeof(int) * (height - 1)) + sizeof(int));
+	int **ptr = (int **)malloc(sizeof(int) * height);
 
 	if (height != 0)
 	{
