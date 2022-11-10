42123320825e638d9017705ac6f5eccf482ab0e7
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-10 13:40:49 -0800

    Updated 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	{
 		for (i = 0; i < height; i++)
 		{
-			ptr[i] = (int *)calloc((width), sizeof(int));
+			ptr[i] = (int *)calloc((width), sizeof(int *));
 		}
 		return (ptr);
 	}
