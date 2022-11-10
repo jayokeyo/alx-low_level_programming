0c96ca81ad757fee890ac278d67d875b2f6daf79
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-10 04:51:05 -0800

    Updated 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,18 +23,28 @@ char *str_concat(char *s1, char *s2)
 	if (len1 + len2 != 0)
 	{
 		ptr = (char *)malloc(len1 + len2 + 1);
-		if (len1 != 0)
+		if (len1 != 0 && len2 != 0)
 		{
 			for (i = 0; i < len1; i++)
 			{
 				*(ptr + i) = *(s1 + i);
 			}
-		}
-		if (len2 != 0)
-		{
 			for (j = len1; j < (len1 + len2); j++)
 			{
 				*(ptr + j) = *(s1 + j);
+			}
+		}
+		else
+		{
+			if (len1 == 0)
+			{
+				for (i = 0; i < len2; i++)
+					*(ptr + i) = *(s2 + i);
+			}
+			else
+			{
+				for (i = 0; i < len1; i++)
+					*(ptr + i) = *(s1 + i);
 			}
 		}
 		return (ptr);
