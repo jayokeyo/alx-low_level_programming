5e26f245bd007d8595fc94dd0c405829bd0326f4
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-11 11:22:55 -0800

    Added 101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies two numbers
+ * @num: array of two integers
+ * Return: num[0] * num[1]
+ */
+int mul(int num[2])
+{
+	if (num[0] == NULL || num[1] == NULL)
+	{
+		print("Error\n");
+		exit(98);
+	}
+}
