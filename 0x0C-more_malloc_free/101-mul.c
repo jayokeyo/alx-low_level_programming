8f98f77a6a6b7fa794795c1bc48bd089316e1cda
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-12 01:16:22 -0800

    Added 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,11 +4,100 @@
  * @num: array of two integers
  * Return: num[0] * num[1]
  */
-int mul(int num[2])
+int mul(int *num)
 {
-	if (num[0] == NULL || num[1] == NULL)
+	n = Number_of_arguments(num);
+	d = is_digit(num);
+
+	if (n != 2 || d != 0 || num[0] == NULL || num[1] == NULL)
 	{
-		print("Error\n");
+		print_string("Error\n");
 		exit(98);
 	}
+	else
+		print_digit(num[0] * num[1]);
+}
+void print_string(char *s)
+{
+	while (*s)
+	{
+		_putchar(*(s + i));
+		i++;
+		s++;
+	}
+}
+void print_digit(int *num)
+{
+	int n, th, hun, tens, rem;
+
+	n = num[i];
+	th = n / 1000;
+	if (th == 0)
+	{
+		rem = n % 1000;
+		hun = rem / 100;
+		if (hun == 0)
+		{
+			rem = rem % 100;
+			tens = rem / 10;
+			if (tens == 0)
+			{
+				_putchar((rem % 10) + '0');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(tens + '0');
+				_putchar((rem % 10) + '0');
+				_putchar('\n');
+			}
+		}
+		else
+		{
+			_putchar(hun + '0');
+			rem = rem % 100;
+			_putchar((rem / 10) + '0');
+			_putchar((rem % 10) + '0');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar(th + '0');
+		rem = n % 1000;
+		_putchar((rem / 100) + '0');
+		rem = rem % 100;
+		_putchar((rem / 10) + '0');
+		_putchar((rem % 10) + '0');
+		_putchar('\n');
+	}
+}
+int Number_of_arguments(int *num)
+{
+	int i = 0, len = 0;
+
+	while (num[i])
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+int is_digit(int *num)
+{
+	int len, n, i, ret;
+	len = Number_of_arguments(num);
+
+	for (i = 0; i < len; i++)
+	{
+		n = *(num + i);
+		if (n >= 0 && n <= 99999)
+			ret = 0;
+		else
+		{
+			ret = 1;
+			break;
+		}
+	}
+	return (ret);
 }
