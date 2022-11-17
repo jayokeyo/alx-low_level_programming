eccb397cd3277d1f2309c2a909ea5bfe65de2d2c
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-17 04:29:01 -0800

    Added 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_name - prints a anme based on the passed function
+ * @name: name to be printed
+ * @f: function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
