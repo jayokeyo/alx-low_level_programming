f4dac493c69db91577c151c0a3f841030e073261
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-17 10:48:02 -0800

    Added 3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * op_add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts a number from another
+ * @a: first number
+ * @b: second number
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides a number by another
+ * @a: first number 
+ * @b: second number
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - finds the remainder after division
+ * @a: first number
+ * @b: second number
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
