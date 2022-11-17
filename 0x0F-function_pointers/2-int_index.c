a0d614d672d17c800b966d7a7ddaf29b990f9bc9
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-17 08:56:50 -0800

    Added 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include "function_pointers.h"
+/**
+ * int_index - applies function cmp on elements of array
+ * @array: array of size size
+ * @size: number of array elements
+ * @cmp: function pointer
+ * Return: index to first matching element
+ */
+int int_index(int *array, int size, int(*cmp)(int))
+{
+	int i, j, ret = -1;
+
+	if (array == NULL || cmp == NULL)
+		return;
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		j = cmp(array[i]);
+		if (j == 1)
+		{
+			ret = i;
+			break;
+		}
+	}
+	return (i);
+}
