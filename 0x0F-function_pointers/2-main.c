88c3cf84f983ff8d4026552df929d70076941254
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-17 09:12:58 -0800

    Added 2-main.c

---
+++ 0x0F-function_pointers/2-main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * is_98 - check if a number is equal to 98
+ * @elem: the integer to check
+ * Return: 0 if false non-zero number otherwise
+ */
+int is_98(int elem)
+{
+	return (98 == elem);
+}
+/**
+ * is_strictly_positive - check if a number is greater than zero
+ * @elem: number to be checked
+ * Return: 0 if false otherwise non zero number
+ */
+int is_strictly_positive(elem)
+{
+	return (elem > 0);
+}
+/**
+ * abs_is_98 - check if the absolute value of a number is 98
+ * @elem: number to be checked
+ * Return: 0 if false otherwise a non zero number
+ */
+int abs_is_98(int elem)
+{
+	return (elem == 98 || -elem == 98);
+}
+/**
+ * main - check the code
+ * Return: Always 0
+ */
+int main(void)
+{
+	int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
+	int index;
+
+	index = int_index(array, 20, is_98);
+	printf("%d\n", index);
+	index = int_index(array, 20, abs_is_98);
+	printf("%d\n", index);
+	index = int_index(array, 20, is_strictly_positive);
+	printf("%d\n", index);
+	return (0);
+}
