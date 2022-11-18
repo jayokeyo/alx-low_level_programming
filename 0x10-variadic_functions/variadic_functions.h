79a21be4de91cd99ac0b0913be1f483a822740da
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-18 11:00:45 -0800

    Updated header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,6 @@
-#ifndef VARIADIC_FUNCTIONS
-#define VARIADIC_FUNCTIONS
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 int sum_them_all(const unsigned int n, ...);
 
-#endif /*VARIADIC_FUNCTIONS*/
+#endif /*VARIADIC_FUNCTIONS_H*/
