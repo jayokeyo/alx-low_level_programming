a0788ff2bad313a8c0f796f935851a984c26ed36
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-18 22:12:03 -0800

    Changed type from unsigned int to int

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,13 +9,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list arg;
-	const unsigned int i, sum = 0;
+	int i, sum = 0;
 
 	if (n == 0)
 		return (0);
 	va_start(arg, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(arg, const unsigned int);
+		sum += va_arg(arg, int);
 	va_end(arg);
 	return (sum);
 }
