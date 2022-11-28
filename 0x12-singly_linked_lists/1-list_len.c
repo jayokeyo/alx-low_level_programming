7353b6b3c9b2313215cd88b3f6702fc9ed48c482
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-28 13:03:24 -0800

    Added 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#include "lists.h"
+/**
+ * list_len - evaluates the number of elements in a linked list
+ * @h: linked list
+ * Return: The number of elements in h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t e = 0;
+	while (*h)
+	{
+		e++;
+		h = h->next;
+	}
+	return (e);
+}
