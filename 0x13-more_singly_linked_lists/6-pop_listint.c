810beb5557ed21af97ed4dd56349f308f1933e39
Julius-Okeyo <jaykopiyo@gmail.com>
2022-12-01 10:30:28 -0800

    Added 6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * pop_listint - removes the first element of a linked list
+ * @head: head of the list;
+ * Return: value of the popped element
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int ret;
+
+	if (head == NULL)
+		return (0);
+	tmp = *head;
+	*head = (*head)->next;
+	ret = tmp->n;
+	free(tmp);
+	return(ret);
+}
