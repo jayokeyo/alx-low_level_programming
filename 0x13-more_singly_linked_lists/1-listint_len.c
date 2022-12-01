7b651801622ddc66e1f5fb94a5f2fb1cec96e48e
Julius-Okeyo <jaykopiyo@gmail.com>
2022-12-01 07:19:00 -0800

    Added 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: pointer to the linked list
+ * Return: number of elements in the linked list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
