4439f9332d200b4608126031d3585d5275754384
Julius-Okeyo <jaykopiyo@gmail.com>
2022-12-01 11:05:45 -0800

    Added 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,9 +9,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	for (i = 0; i < index; i++)
-		head = head->next;
 	if (head == NULL)
 		return (NULL);
+	for (i = 0; i <= index; i++)
+	{
+		head = head->next;
+		if (head == NULL && i != index)
+			return (NULL);
+	}
 	return (head);
 }
