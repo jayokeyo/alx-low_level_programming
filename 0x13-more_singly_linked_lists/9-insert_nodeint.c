f0b72461151063af99c6b9851dc5d7616ccaee1d
Julius-Okeyo <jaykopiyo@gmail.com>
2022-12-01 11:54:11 -0800

    Added 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,10 +16,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	if (*head == NULL)
 		return (NULL);
+	if (idx == 0)
+	{
+		new->n = n;
+		new->next = tmp;
+		*head = new;
+		return (new);
+	}
 	for (i = 0; i < idx; i++)
+	{
 		*head = (*head)->next;
 		if (*head == NULL && i != (idx - 1))
 			return (NULL);
+	}
 	tmp = (*head)->next;
 	(*head)->next = new;
 	new->n = n;
