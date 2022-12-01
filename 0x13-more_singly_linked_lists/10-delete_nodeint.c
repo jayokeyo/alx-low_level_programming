77c2fb5c23055696a5c52cdbd07f55a5eaf34d00
Julius-Okeyo <jaykopiyo@gmail.com>
2022-12-01 13:50:51 -0800

    Added 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (*head == NULL)
 		return (-1);
-	for (i = 0; i < (index - 1); i++)
+	for (i = 0; i < index; i++)
 	{
 		*head = (*head)->next;
 		if (*head == NULL && i != (index - 1))
