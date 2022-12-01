819500396e17e93c367bf0c53274253f2a53113e
Julius-Okeyo <jaykopiyo@gmail.com>
2022-12-01 09:47:28 -0800

    Updated 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,16 +3,17 @@
  * free_listint2 - frees memory allocated to linked list
  * @head: head of linked list
  */
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	while (head)
+	while (*head)
 	{
-		tmp = head->next;
-		free(head);
-		head = tmp;
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
 	}
+	free(*head);
 	head = NULL;
 
 }
