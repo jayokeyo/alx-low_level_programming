ff3af5822dd82c4bcab0587a3cafffd516db8bb4
Julius-Okeyo <jaykopiyo@gmail.com>
2022-12-07 07:07:20 -0800

    Added 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+/**
+ * create_file - creates a file
+ * @filename: file name
+ * @text_content: NULL terminator byte
+ * Return: 1 (Success) else -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len;
+	char *emp = "";
+
+	len = strlen(text_content) + 1;
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = emp;
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	write(fd, text_content, len);
+	return (1);
+}
