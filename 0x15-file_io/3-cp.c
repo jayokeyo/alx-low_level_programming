6afd11a09a7efc7e52aa0d2c1058f905bd86b648
Julius-Okeyo <jaykopiyo@gmail.com>
2022-12-07 09:52:46 -0800

    Updated 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to.\n");
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd1 = open(argv[1], O_RDONLY);
@@ -21,25 +21,25 @@ int main(int argc, char *argv[])
 	rd = read(fd1, buf, 1024);
 	if (fd1 == -1 || rd == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s.\n", argv[1]);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	wr = write(fd2, buf, rd);
 	if (fd2 == -1 || wr == -1)
 	{
-		dprintf(2, "Error: Can't write to %s.\n", argv[2]);
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	cl1 = close(fd1);
 	cl2 = close(fd2);
 	if (cl1 == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d.\n", fd1);
+		dprintf(2, "Error: Can't close fd %d\n", fd1);
 		exit(100);
 	}
 	if (cl2 == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d.\n", fd2);
+		dprintf(2, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
 	return (0);
