ba2faa48103bf0ea09ff2fe3a889e6e33b0c863b
Julius-Okeyo <jaykopiyo@gmail.com>
2022-12-07 06:27:40 -0800

    Updated main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 size_t read_textfile(const char *filename, size_t letters);
