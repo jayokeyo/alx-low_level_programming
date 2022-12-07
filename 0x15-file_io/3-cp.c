6c3239404694d2b00bdae9211906c2d320cd2374
Julius-Okeyo <jaykopiyo@gmail.com>
2022-12-07 09:56:48 -0800

    Updated 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
-		exit (97);
+		exit(97);
 	}
 	fd1 = open(argv[1], O_RDONLY);
 	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_RDWR, 0664);
@@ -22,25 +22,25 @@ int main(int argc, char *argv[])
 	if (fd1 == -1 || rd == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit (98);
+		exit(98);
 	}
 	wr = write(fd2, buf, rd);
 	if (fd2 == -1 || wr == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit (99);
+		exit(99);
 	}
 	cl1 = close(fd1);
 	cl2 = close(fd2);
 	if (cl1 == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd1);
-		exit (100);
+		exit(100);
 	}
 	if (cl2 == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd2);
-		exit (100);
+		exit(100);
 	}
 	return (0);
 }
