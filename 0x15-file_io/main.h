20895764dc52bb5aba77b05dacc3cb98bd5297ef
Julius-Okeyo <jaykopiyo@gmail.com>
2022-12-07 12:09:35 -0800

    Updated main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,9 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void Read_Error(char *f_name);
+void Write_Error(char *f_name);
+void Arg_Error(void);
+void Close_Error(int fd);
 
 #endif /*MAIN_H*/
