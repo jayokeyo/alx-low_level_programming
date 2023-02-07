4201b2a9a516a85a61e1018fa2a87dedc0b85b17
Julius-Okeyo <jaykopiyo@gmail.com>
2023-02-07 06:52:29 -0800

    Added 3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to a hash table
+ * @ht: pointer to hash table
+ * @key: key of item to be added
+ * @value: value of item to be added
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+{
+	unsigned long Index;
+	hash_node_t *new;
+	hash_node_t *head;
+
+	Index = hash_djb2(key) % size;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	new->key = malloc(strlen(*key) + 1);
+	if (new->key == NULL)
+		return (0);
+	strcpy(new->key, key);
+	new->value = malloc(strlen(*value) + 1);
+	if (new->value == NULL)
+		return (0);
+	strcpy(new->value, value);
+	strcpy(new->next, NULL);
+
+	if (ht->array[Index] == NULL)
+		ht->array[Index] == new;
+	else
+	{
+		if (strcmp(ht->array[Index]->key, key) == 0)
+			strcpy(ht->array[Index]->value, value);
+	}
+}
