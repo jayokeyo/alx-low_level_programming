9f8fa38423ae2c2328aa54618116d2c2ebab2b68
Julius-Okeyo <jaykopiyo@gmail.com>
2023-02-07 07:55:16 -0800

    Replaced 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,45 +3,47 @@
 #include <stddef.h>
 #include <string.h>
 /**
- * hash_table_set - adds an element to a hash table
- * @ht: pointer to hash table
- * @key: key of item to be added
- * @value: value of item to be added
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ * Return: Upon failure - 0. Otherwise - 1.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int i, Index;
 	hash_node_t *new;
-
-	Index = key_index((const unsigned char *)key, ht->size);
-
+	char *value_copy;
+	unsigned long int index, i;
+	
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
+	value_copy = strdup(value);
+	if (value_copy == NULL)
 		return (0);
-	new->key = malloc(strlen(key) + 1);
-	if (new->key == NULL)
-		return (0);
-	strcpy(new->key, key);
-	new->value = malloc(strlen(value) + 1);
-	if (new->value == NULL)
-		return (0);
-	strcpy(new->value, value);
-	new->next = NULL;
-
-	if (ht->array[Index] == NULL)
-		ht->array[Index] = new;
-	else
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
-		for (i = Index; ht->array[i]; i++)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			if (strcmp(ht->array[i]->key, key) == 0)
-			{
-				strcpy(ht->array[i]->value, value);
-			}
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
 		}
 	}
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 	return (1);
 }
