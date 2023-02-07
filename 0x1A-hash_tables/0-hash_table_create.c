78d788128b03348b9bb5b8f75c5204594bd3fcd7
Julius-Okeyo <jaykopiyo@gmail.com>
2023-02-07 04:32:28 -0800

    Updated 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,7 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 #include <stddef.h>
+#include <ctype.h>
 /**
  * hash_table_create - creates a hash table
  * @size: size of the hash table
@@ -11,6 +12,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long i;
 	hash_table_t *hash_table;
 
+	if (size == 0 || isdigit(size) == 0)
+		return NULL;
 	hash_table = malloc(sizeof(hash_node_t));
 	if (hash_table == NULL)
 		return NULL;
