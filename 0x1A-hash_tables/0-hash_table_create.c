1e3107540e99e86281a2c6888b8a46ba5be93d07
Julius-Okeyo <jaykopiyo@gmail.com>
2023-02-07 04:39:01 -0800

    Updated 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long i;
 	hash_table_t *hash_table;
 
-	if (size == 0 || isdigit(size) == 0)
+	if (size == 0)
 		return NULL;
 	hash_table = malloc(sizeof(hash_node_t));
 	if (hash_table == NULL)
