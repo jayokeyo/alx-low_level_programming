fdf7fb4ad9ce5b49b5a9e4c1086552017a43644f
Julius-Okeyo <jaykopiyo@gmail.com>
2023-07-14 05:03:03 -0700

    Added 102-interpolation.c

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,37 @@
+#include "search_algos.h"
+
+/**
+  * interpolation_search - uses interpolation search algorithm to locate a value
+  * @array: array to iterate over
+  * @size: size of array
+  * @value: The value to search for.
+  * Return: Index of value within the array(Success) otherwise -1
+  */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t i, j, k;
+
+	if (array == NULL)
+		return (-1);
+
+	for (j = 0, k = size - 1; k >= l;)
+	{
+		i = j + (((double)(k - j) / (array[k] - array[j])) * (value - array[j]));
+		if (i < size)
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		else
+		{
+			printf("Value checked array[%ld] is out of range\n", i);
+			break;
+		}
+
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			k = i - 1;
+		else
+			j = i + 1;
+	}
+
+	return (-1);
+}
