8d1e5a61313fe2913311dc501ffb4accbe6eabeb
Julius-Okeyo <jaykopiyo@gmail.com>
2023-07-14 04:48:31 -0700

    Updated search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,8 +4,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stddef.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif /*SEARCH_ALGOS_H*/
